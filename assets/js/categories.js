dc3c685d084fd0a240347b9683c29b59a8b63b37
Sharad Raj <dev_sharad@outlook.com>
2021-11-06 06:52:31 +0000

    Change path

---
+++ assets/js/categories.js
@@ -0,0 +1,32 @@
+---
+---
+
+const categories = { {% for category in site.categories %}{% capture category_name %}{{ category | first }}{% endcapture %}{{ category_name }}: [{% for post in site.categories[category_name] %}{ url: `{{ site.baseurl }}{{ post.url }}`, date: `{{post.date | date_to_string}}`, title: `{{post.title}}`},{% endfor %}],{% endfor %} }
+
+window.onload = function () {
+  document.querySelectorAll(".category").forEach((category) => {
+    category.addEventListener("click", function (e) {
+      const posts = categories[e.target.innerText];
+      let html = ``
+      posts.forEach(post=>{
+        html += `
+        <a class="modal-article" href="${post.url}">
+          <h4>${post.title}</h4>
+          <small class="modal-article-date">${post.date}</small>
+        </a>
+        `
+      })
+      document.querySelector("#category-modal-title").innerText = e.target.innerText;
+      document.querySelector("#category-modal-content").innerHTML = html;
+      document.querySelector("#category-modal-bg").classList.toggle("open");
+      document.querySelector("#category-modal").classList.toggle("open");
+    });
+  });
+
+  document.querySelector("#category-modal-bg").addEventListener("click", function(){
+    document.querySelector("#category-modal-title").innerText = "";
+    document.querySelector("#category-modal-content").innerHTML = "";
+    document.querySelector("#category-modal-bg").classList.toggle("open");
+    document.querySelector("#category-modal").classList.toggle("open");
+  })
+};
