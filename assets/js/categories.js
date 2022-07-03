472b06b79599cb77ba78530c7d765abab2eb0620
Sharad Raj <dev_sharad@outlook.com>
2022-07-03 09:15:42 +0530

    Fixed categories with spaces in their names.

--- assets/js/categories.js
+++ assets/js/categories.js
@@ -1,12 +1,14 @@
 ---
 ---
 
-const categories = { {% for category in site.categories %}{% capture category_name %}{{ category | first }}{% endcapture %}{{ category_name }}: [{% for post in site.categories[category_name] %}{ url: `{{ site.baseurl }}{{ post.url }}`, date: `{{post.date | date_to_string}}`, title: `{{post.title}}`},{% endfor %}],{% endfor %} }
+const categories = { {% for category in site.categories %}{% capture category_name %}{{ category | first }}{% endcapture %}{{ category_name | replace: " ", "_" }}: [{% for post in site.categories[category_name] %}{ url: `{{ site.baseurl }}{{ post.url }}`, date: `{{post.date | date_to_string}}`, title: `{{post.title}}`},{% endfor %}],{% endfor %} }
+
+console.log(categories)
 
 window.onload = function () {
   document.querySelectorAll(".category").forEach((category) => {
     category.addEventListener("click", function (e) {
-      const posts = categories[e.target.innerText];
+      const posts = categories[e.target.innerText.replace(" ","_")];
       let html = ``
       posts.forEach(post=>{
         html += `
